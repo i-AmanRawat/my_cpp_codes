b4f845e5d4ee35c78de9ba614aa335f616877f5b
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-03-13 00:00:59 +0530

    Create fact_using_recursion.cpp

---
+++ lecture_ques/fact_using_recursion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int factorial (int n){
+    if (n==0 || n==1){
+        return 1;
+    }
+
+    else if (n>1){
+        return n * factorial(n-1);
+    }
+
+    else {
+        return 0;
+    }
+    
+}
+int main (){
+    int n;
+    cout << "Enter a number to calculate it's factorial : "<< endl;
+    cin >> n ;
+    cout << " Factorial of " << n << " is " << factorial(n)<< endl;
+    return 0 ;
+}
