80314bd965aedb22157f5cec4fa225dacf1251b9
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-03-13 13:38:25 +0530

    Create privateprop.cpp

---
+++ lecture_ques/privateprop.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std ;
+class calculator 
+{
+private:
+    int a , b;
+public:
+    int p,q ;
+    int sum(int p, int q){
+        return p+q
+        ;
+    }
+    int mul(int a , int b);
+};
+
+int calculator::mul(int a, int b){
+    return m*n;
+}
+
+int main(){
+    int a,b;
+    calculator calculation;
+    calculation.a=2;
+    calculation.b=6;
+    cout << "The sum of 4 and 6 is "<< calculation.sum(4,6)<<endl;
+    cout << "The multiplication of 4 and 6 is "<< calculation.mul(a,b)<<endl;
+
+    return 0; 
+}
