9f5557e51a7ce66c21ce31585e6a03b3735fad43
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-03-21 22:16:15 +0530

    Create swapwithout3rdvariable.cpp

---
+++ lecture_ques/swapwithout3rdvariable.cpp
@@ -0,0 +1,19 @@
+ #include <iostream>
+using namespace std;
+void swap (int &a, int &b){
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+}
+int main (){
+    int x = 4;
+    int y = 2;
+    cout <<" Value of x is (before swap function) : "<< x << endl;
+    cout <<" Value of y is (before swap function) : " << y << endl;
+    swap (x,y);
+    cout <<" Value of x is (after passing value into the swap function) : "<< x << endl;
+    cout <<" Value of y is (after passing value into the swap function) : " << y << endl;
+
+    return 0 ;
+}
