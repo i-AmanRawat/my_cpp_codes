1f480d5c663ff20044f6d8144718f54c84aa1036
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-04-06 00:24:12 +0530

    Update parameterized_friend_exmple.cpp

--- lecture_ques/parameterized_friend_exmple.cpp
+++ lecture_ques/parameterized_friend_exmple.cpp
@@ -3,24 +3,23 @@
 using namespace std ;
 class points{
     int x ,y;
-    int dis;
 
     public:
         points(int m , int n){
             x = m;
             y = n;
         }
-    friend int distance (points , points );
+    friend float distance (points , points );
 
 };
 
-int distance (points p1 , points p2 ){
+float distance (points p1 , points p2 ){
     return  (sqrt(pow((p2.x - p1.x),2) + pow((p2.y - p1.y),2)));
 }
 
 int main (){
     points A(2,-4),B(10,-10);
-    int length = distance(A,B);
+    float length = distance(A,B);
     cout << "The separation between point p1 and p2 is "<< length<<endl;
     return 0;
 }
