cda3d69761de50ac886c046dcc0ea6eefa61e693
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-05-10 14:20:44 +0530

    Create virtual_class.cpp

---
+++ lecture_ques/virtual_class.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std ;
+class student {
+    protected :
+        int roll_number ;
+    public :
+        void set_rollno(int r ){
+            roll_number = r;
+        }
+        void print_rollno(void){
+            cout << "Your Roll Number is : "<< roll_number<< endl;
+        }
+};
+
+class test : virtual public student {
+    protected :
+        float physics;
+        float maths ;
+    public : 
+        void set_marks (float m1 , float m2){
+            physics = m1;
+            maths = m2;
+        }
+        void print_marks(void ){
+            cout << "You have obtained :"<<endl
+                 << physics
+                 << " marks in physics."<< endl
+                 << maths
+                 << " marks in maths."<< endl;
+        }
+};
+
+class sports : virtual public student{
+    protected:
+        float score;
+    public :
+        void set_score(float sc){
+            score = sc;
+        }
+        void print_score(void){
+            cout << "Your score in sport is "<<score<< endl;
+        }
+};
+
+class result : public test, public sports{
+    protected :
+        float total;
+    public :
+        void display (void){
+            total = physics + maths + score;
+            print_rollno();
+            print_marks();
+            print_score();
+            cout <<"In total you have scored "<< total<< " marks."<<endl;
+        }
+};
+
+
+int main (){
+    result s1;
+    s1.set_rollno(01);
+    s1.set_marks(95.5,99.5);
+    s1.set_score(90);
+    s1.display();
+}
