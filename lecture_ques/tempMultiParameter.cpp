560d2a78b603906effc4607faa51a2819ac4189e
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-05-15 21:44:38 +0530

    Update tempMultiParameter.cpp

--- lecture_ques/tempMultiParameter.cpp
+++ lecture_ques/tempMultiParameter.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-template <class T1, class T2>
+template <class T1=int, class T2=string>    //here i have setted default datatype and like this we can define more multiple parameters.
 class numbers{
     T1 data1;
     T2 data2;
@@ -20,4 +20,7 @@ class numbers{
 int main (){
     numbers <int , float> num(2,3.4);
     num.display();
+    cout<<endl;
+    numbers<> num2(4,"aman");
+    num2.display();
 }
