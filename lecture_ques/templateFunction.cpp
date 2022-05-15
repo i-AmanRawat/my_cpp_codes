fb58a52087b66e827e363b2052216bdf99c5c0ef
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2022-05-15 22:15:38 +0530

    Update templateFunction.cpp

--- lecture_ques/templateFunction.cpp
+++ lecture_ques/templateFunction.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
 using namespace std;
-//template function so that with the help of a simgle function we can deal with different datatypes 
+//function overloading 
+//template function so that with the help of a single function we can deal with different datatypes 
 template <class T1, class T2>
 void average (T1 a, T2 b){
     float avg = (a+b)/2.0;
+    cout << "Template waala call huwa !"<< endl;
+    cout <<avg<<endl;
+}
+
+void average (int a, int b){
+    float avg = (a+b)/2.0;
+    cout << "simple waala call huwa !"<< endl;
     cout <<avg<<endl;
 }
 
 int main(){
+    average(100,50);    //Exact match takes the higher precedence
     average (4,5.5);
 }
